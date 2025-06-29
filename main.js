74adc0b65ff52646eed7a0963995057ef083b2e7
cindy kivala <cindykivala@gmail.com>
2025-06-29 16:58:09 +0300

    liker code

--- main.js
+++ main.js
@@ -3,7 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts = document.querySelectorAll('.like-glyph');
+const modal = document.getElementById('modal');
+const modalMessage = document.getElementById('modal-message');
 
+//hide modal on load
+modal.classList.add('hidden');
+//Add event listeners
+hearts.forEach(heart => {
+  heart.addEventListener('click', () => {
+    mimicServerCall()
+      .then(() => {
+        if (heart.textContent === EMPTY_HEART) {
+          heart.textContent = FULL_HEART;
+          heart.classList.add('activated-heart');
+        } else {
+          heart.textContent = EMPTY_HEART;
+          heart.classList.remove('activated-heart');
+        }
+      })
+      .catch((error) => {
+        modal.classList.remove('hidden');
+        modalMessage.textContent = error;
+        setTimeout(() => modal.classList.add('hidden'), 3000);
+      });
+  });
+});
 
 
 
